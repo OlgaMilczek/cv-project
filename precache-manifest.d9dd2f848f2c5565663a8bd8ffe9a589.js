0b403b5802f087dc43154145666d90e0c46cd905
Olga Nowakowska <o.w.nowakowska@gmail.com>
2020-10-22 12:19:11 +0200

    Updates

---
+++ precache-manifest.d9dd2f848f2c5565663a8bd8ffe9a589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1231914b388262936e4f4a73000ea34",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "50f63fa8f4657ba04ffb",
+    "url": "/cv-project/static/js/2.c9b75bd1.chunk.js"
+  },
+  {
+    "revision": "857af1c517e1f86bdb841a8a5ccc520a",
+    "url": "/cv-project/static/js/2.c9b75bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c264f5bd888f5b8e2d5b",
+    "url": "/cv-project/static/js/main.12a266c5.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
